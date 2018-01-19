d1ce6f87d427b2c592ce71a8af5d17b7d97d9072
Alexander Austin <austinag@g.cofc.edu>
2018-01-18 21:57:03 -0500

    first commit

---
+++ app/src/main/java/com/example/alex/simpletimer/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.alex.simpletimer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private MyCountDownTimer countDownTimer;
+    private long timeElapsed;
+    private boolean timerHasStarted = false;
+    private Button startB;
+    private TextView text;
+    private TextView timeElapsedView;
+    private final long startTime = 50 * 1000;
+    private final long interval = 1 * 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startB = (Button) this.findViewById(R.id.button);
+        startB.setOnClickListener(this);
+        text = (TextView) this.findViewById(R.id.timer);
+        timeElapsedView = (TextView) this.findViewById(R.id.timeElapsed);
+        countDownTimer = new MyCountDownTimer(startTime, interval);
+        text.setText(text.getText() + String.valueOf(startTime));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(!timerHasStarted) {
+            countDownTimer.start();
+            timerHasStarted = true;
+            startB.setText(getString(R.string.start_text));
+        } else {
+            countDownTimer.cancel();
+            timerHasStarted = false;
+            startB.setText(getString(R.string.reset_text));
+        }
+
+    }
+
+
+    public class MyCountDownTimer extends CountDownTimer{
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            text.setText(R.string.time_remain + String.valueOf(millisUntilFinished));
+            timeElapsed = startTime - millisUntilFinished;
+            timeElapsedView.setText(R.string.time_elapse + String.valueOf(timeElapsed));
+
+        }
+
+        @Override
+        public void onFinish() {
+            text.setText(R.string.time_up);
+            timeElapsedView.setText(R.string.time_elapse + String.valueOf(startTime));
+
+        }
+
+    }
+
+}
